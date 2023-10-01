ed94136955b1bedca462c4126bb58a61a15cd2e1
Vyacheslav Ikonnikov <vyacheslav.ikonnikov@cyberia.studio>
2023-10-01 16:38:52 +0700

    esc keydown event, fixes

--- src/runtime/components/FrogModal.ts
+++ src/runtime/components/FrogModal.ts
@@ -1,5 +1,5 @@
 import './modal.css';
-import {h, useState} from "#imports";
+import {h, onMounted, onUnmounted, useState} from "#imports";
 import {DefineComponent} from "vue";
 import {useFrogModal} from "../composables/useFrogModal";
 
@@ -8,10 +8,22 @@ export const FrogModal = {
         const modal = useState<{ component: DefineComponent | {}; options: any } | null>('frog-modal');
         const [, closeModal] = useFrogModal();
 
+        const onKeyDown = (e: KeyboardEvent) => {
+            if (e.keyCode === 27) closeModal();
+        }
+
+        onMounted(() => {
+            document.addEventListener('keydown', onKeyDown)
+        })
+
+        onUnmounted(() => {
+            document.removeEventListener('keydown', onKeyDown)
+        })
+
         return () =>
             h('div', { class: modal.value ? 'frog-modal' : 'frog-modal hide' }, [
                 h('div', { class: 'frog-modal__overlay', onClick: closeModal }),
-                h('div', { class: 'frog-modal__content' }, [h(modal.value?.component ?? '', modal.value?.options ? { ...modal.value.options } : {}, )]),
+                h('div', { class: 'frog-modal__content' }, [ h(modal.value?.component ?? 'span', modal.value?.options ? { ...modal.value.options } : {}, )]),
             ]);
     },
 };
--- src/runtime/composables/useFrogModal.ts
+++ src/runtime/composables/useFrogModal.ts
@@ -1,10 +1,10 @@
 import {DefineComponent} from "vue";
-import {useState} from "#imports";
+import {markRaw, useState} from "#imports";
 
 export const useFrogModal = <T>(): [(comp: DefineComponent | {}, opts?: T) => void, () => void] => {
     const modal = useState<{ component: DefineComponent | {}; options: T | {} } | null>('frog-modal')
 
-    const setter = (_cmp: DefineComponent | {}, _opts: T | {} = {}) => modal.value = {component: _cmp, options: _opts};
+    const setter = (_cmp: DefineComponent | {}, _opts: T | {} = {}) => modal.value = {component: markRaw(_cmp), options: _opts};
     const clearer = () => modal.value = null;
 
     return [setter, clearer];
